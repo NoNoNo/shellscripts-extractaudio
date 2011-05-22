e1c73380f104bc953d0dd1a5db16892fb664b854
Oliver Prygotzki <oliver-github@prygotzki.de>
2011-05-22 20:24:26 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,32 @@
+`extractaudio.sh` extracts the audio portion of a video file.
+
+This is done without transcoding by extracting the native audio track (auto-detected as `MP3` or `M4A`) from container formats like `MOV`/`AVI`/`FLV`/`M4V`/`MP4`.
+
+Syntax: `extractaudio INPUT.ext`
+
+The audio track is saved as `INPUT.ext.mp3` or `INPUT.ext.m4a`.
+
+This will of course work only, if the video file contains an audio track in `MP3` or `M4A`/`AAC` format.
+`MOV`/`AVI`/`FLV` files may contain `MP3` or `M4A`/`AAC`, `MP4` or `M4V` files contain only `M4A`=`AAC`.
+
+Requires: 
+
+* [`ffmpeg`](http://www.ffmpeg.org/)
+* [`MP4Box`](http://www.tkn.tu-berlin.de/research/evalvid/) (only used for `M4A`; without it iTunes will not recognise the `M4A`)
+
+Examples:
+
+You have some videos from *YouTube* and want to extract the audio track without transcoding.
+
+* To convert `FLV` to `MP3`: `audioextract video.flv`
+* To convert `MP4` to `M4A`: `audioextract video.mp4`
+
+Install:
+
+    sudo mv extractaudio.sh /usr/local/bin/extractaudio
+    sudo chmod 755 /usr/local/bin/extractaudio
+
+See also:
+
+* [Wikipedia: *Comparison of container formats*](http://en.wikipedia.org/wiki/Comparison_of_container_formats)
+* [Easy YouTube Video Downloader](http://www.yourvideofile.com/) is a free addon for Firefox, Google Chrome, Opera and Safari that adds a nice download button to every YouTube page
---
+++ extractaudio.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+if [ "$1" == "" ] ; then
+  echo
+  echo "extractaudio 1.0/5.3.2011 by Oliver Prygotzki"
+  echo "https://github.com/NoNoNo/shellscripts-extractaudio"
+  echo
+  echo "Extracts the audio track of a video file without transcoding:"
+  echo "MP3 or M4A from container formats like wie MOV/AVI/FLV/M4V/MP4,"
+  echo "with autodetection. Syntax: extractaudio INPUT.ext"
+  echo "The audio track is saved as INPUT.ext.mp3 or INPUT.ext.m4a"
+  echo 
+  exit
+fi
+
+AUDIOCODEC=$(ffmpeg -i "$1" 2>&1 | grep Stream | grep Audio)
+
+echo "${AUDIOCODEC}" | grep mp3 &> /dev/null
+if [ "$?" -eq "0" ]; then
+  nice ffmpeg -loglevel 0 -i "$1" -vn -acodec copy "$1".mp3
+  if [ "$?" -eq "0" ]; then
+    STATUSTEXT="Saved as $1.mp3"
+  else
+    STATUSTEXT="FFmpeg quit unsuccessfully!"
+  fi  
+else
+  echo "${AUDIOCODEC}" | grep aac &> /dev/null
+  if [ "$?" -eq "0" ]; then
+    nice ffmpeg -loglevel 0 -i "$1" -vn -acodec copy "$1".temp.aac
+    if [ "$?" -eq "0" ]; then
+      nice MP4Box -add "$1".temp.aac "$1".m4a
+      rm "$1".temp.aac
+      STATUSTEXT="Saved as $1.m4a"
+    else
+      STATUSTEXT="FFmpeg quit unsuccessfully!"
+    fi
+  else 
+    echo "${AUDIOCODEC}" | grep Audio &> /dev/null
+    if [ "$?" -eq "0" ]; then
+      STATUSTEXT="This script can only extract MP3 and AAC audio tracks!"
+    else
+      AUDIOCODEC="- Did not find audio track! -"
+      STATUSTEXT=""
+    fi
+  fi
+fi
+
+echo
+echo "The video contains this audio track:"
+echo "${AUDIOCODEC}"
+echo "${STATUSTEXT}"
+echo
